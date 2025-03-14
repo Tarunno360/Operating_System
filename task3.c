9b47cde97fdb02411890028dc62d29f343d255ac
Tarunno360 <sm.azmain.faysal@g.bracu.ac.bd>
2025-03-15 03:07:55 +0600

    changes in task3

--- task3.c
+++ task3.c
@@ -6,7 +6,7 @@
 
 int process_count = 1;  
 
-void create_extra_child_if_needed() {
+void odd_pid_child_creation() {
     if (getpid() % 2 != 0) {  
         pid_t new_child = fork();
         if (new_child > 0) {
@@ -18,19 +18,16 @@ void create_extra_child_if_needed() {
 int main() {
     pid_t a, b, c;
 
-    
     a = fork();
     if (a > 0) process_count++;
 
-    
     b = fork();
     if (b > 0) process_count++;
 
     c = fork();
     if (c > 0) process_count++;
-
-    create_extra_child_if_needed();
-    sleep(1); 
+    odd_pid_child_creation();
+    while (wait(NULL)>0);
     if (getppid() != 1) {  
         printf("Total processes created: %d\n", process_count);
     }
