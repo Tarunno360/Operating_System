daad29404f0ed528fd0b08bfabcded4d279aecc6
Tarunno360 <sm.azmain.faysal@g.bracu.ac.bd>
2025-05-11 02:35:46 +0600

    test1 of fina_aumio_question solved

--- Practice_for_final/test1.c
+++ Practice_for_final/test1.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
 
-int main(){
-    int x,y;
-    char str[50];
-    FILE *fp;
-    fp=fopen("in.txt","r");
-    fgets(str,50,fp);
-    printf("%s\n",str);
-    fclose(fp);
+int main() {
+    int inp1,inp2,answer1,i;
+    scanf("%d %d",&inp1,&inp2);
+    if (inp1>=inp2){
+        for (i=1;i<=inp2;i++){
+            if (inp1%i==0 && inp2%i==0){
+                answer1=i;
+            }
+        }
+    }
+    else{
+        for (i=1;i<=inp1;i++){
+            if (inp1%i==0 && inp2%i==0){
+                answer1=i;
+            }
+    }
+    }
+    printf("The GCD of %d and %d is %d\n", inp1, inp2, answer1);
+    return 0;
 }
